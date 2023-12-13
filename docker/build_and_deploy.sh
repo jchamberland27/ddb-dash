6a17f481a62f4d5a007a228ce8f219c8002a1801
Josh <jchamberland27@gmail.com>
2023-12-13 03:43:40 +0000

    db mount complete

--- docker/build_and_deploy.sh
+++ docker/build_and_deploy.sh
@@ -1,10 +1,14 @@
 # utility script to build docker image
 echo "Build and deploy ddb-dash docker image..."
 
+# clear out any previous versions
+docker stop ddb-dash
+docker rm ddb-dash
+
 #set $1 to default port if not passed in
 if [ -z "$1" ]
   then
-    echo "No port argument supplied, using default port 5000"
+    echo "No port argument supplied, using default port 5001"
     set -- "5001"
 fi
 
@@ -14,7 +18,7 @@ cd ../src
 
 # build and run docker image on port passed into script
 docker image build -t ddb-dash .
-docker run -d -p $1:5001 ddb-dash
+docker run -d --name ddb-dash -v ddb-dash:/app/db -p $1:5001 ddb-dash
 
 #cleanup
 rm Dockerfile
--- src/dash.py
+++ src/dash.py
@@ -3,18 +3,30 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO
 import json
+import os
+import pickledb
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+db = pickledb.load('db/db.db', False)
+if db is None:
+    print("Failed to load DB, exiting")
+    exit(1)
+
+
 @app.route('/')
 def display_ui():
-    """Display the UI for the Deck Roller."""
-    return render_template('index.html')
+    """Display the UI for the dash."""
+    return "UI Pending"
+    #return render_template('index.html')
+
 
 @app.route('/ping')
 def ping():
-    return 'PONG'
+    """Ping the service."""
+    return 'pong'
+
 
 #Start app
 if __name__ == '__main__':
